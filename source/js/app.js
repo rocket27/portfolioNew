de8c16fa1549558beca115ff522413dfa0c3f288
Dmitry Kamenev <dmitry.kamenev.27@gmail.com>
2017-10-26 22:19:37 +1000

    Added svg icons, map and user contacts block

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+ymaps.ready(init);
+var myMap;
+
+function init() {     
+  myMap = new ymaps.Map("map", {
+      center: [55.02378694, 82.90917014],
+      zoom: 15
+  });
+
+  myMap.behaviors.disable('scrollZoom');
+}

e7f298c540b2f12c44be4b9b8309c2c106cc5a36
Dmitry Kamenev <dmitry.kamenev.27@gmail.com>
2017-10-27 02:07:55 +1000

    Minor changes in the template and layout

--- source/js/app.js
+++ source/js/app.js
@@ -1,13 +1,21 @@
 'use strict';
 
 ymaps.ready(init);
-var myMap;
+var myMap,
+    myPlacemark;
 
 function init() {     
   myMap = new ymaps.Map("map", {
-      center: [55.02378694, 82.90917014],
-      zoom: 15
+    center: [55.01413298, 82.92389704],
+    zoom: 17
   });
 
   myMap.behaviors.disable('scrollZoom');
+
+  myPlacemark = new ymaps.Placemark([55.01352878, 82.92876793], { 
+    hintContent: 'ул. Инская, д. 5', 
+    balloonContent: 'Офис 115' 
+  });
+
+  myMap.geoObjects.add(myPlacemark);
 }

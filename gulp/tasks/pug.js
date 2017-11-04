d5249b1b30b6e5446b622e5fe966146f16e50113
Dmitry Kamenev <dmitry.kamenev.27@gmail.com>
2017-11-05 02:14:21 +1000

    Added welcome page, new pug modules, new styles

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -2,8 +2,13 @@
 
 module.exports = function() {
   $.gulp.task('pug', function() {
+    let content = require('./../../source/json/content.json');
+
     return $.gulp.src('./source/template/pages/*.pug')
-      .pipe($.gp.pug({ pretty: true }))
+      .pipe($.gp.pug({
+        locals: content,
+        pretty: true
+      }))
       .on('error', $.gp.notify.onError(function(error) {
         return {
           title: 'Pug',
--- source/js/global.js
+++ source/js/global.js
@@ -1,21 +1,21 @@
-'use strict';
+// 'use strict';
 
-ymaps.ready(init);
-var myMap,
-    myPlacemark;
+// ymaps.ready(init);
+// var myMap,
+//     myPlacemark;
 
-function init() {     
-  myMap = new ymaps.Map("map", {
-    center: [55.01413298, 82.92389704],
-    zoom: 17
-  });
+// function init() {     
+//   myMap = new ymaps.Map("map", {
+//     center: [55.01413298, 82.92389704],
+//     zoom: 17
+//   });
 
-  myMap.behaviors.disable('scrollZoom');
+//   myMap.behaviors.disable('scrollZoom');
 
-  myPlacemark = new ymaps.Placemark([55.01352878, 82.92876793], { 
-    hintContent: 'ул. Инская, д. 5', 
-    balloonContent: 'Офис 115' 
-  });
+//   myPlacemark = new ymaps.Placemark([55.01352878, 82.92876793], { 
+//     hintContent: 'ул. Инская, д. 5', 
+//     balloonContent: 'Офис 115' 
+//   });
 
-  myMap.geoObjects.add(myPlacemark);
-}
+//   myMap.geoObjects.add(myPlacemark);
+// }

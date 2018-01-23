3853a5d8b777e039c6f2c83f3766bbe730fa2b64
Dmitry Kamenev <dmitry.kamenev.27@gmail.com>
2018-01-23 13:37:16 +1000

    Added flip effect, written flip logic, authorization form completed

--- gulp/paths/source.js
+++ gulp/paths/source.js
@@ -4,5 +4,6 @@ module.exports = [
   './source/js/global.js',
   './source/js/parallax.js',
   './source/js/scrollParallax.js',
-  './source/js/preloader.js'
+  './source/js/preloader.js',
+  './source/js/flipCards.js'
 ];
---
+++ source/js/flipCards.js
@@ -0,0 +1,39 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', function() {
+  let flipBox = document.querySelector('.flipBox');
+  
+  if (flipBox) {
+    
+    let autBtn = document.querySelector('.authorization'),
+      userCard = document.querySelector('.flipBox__card_user'),
+      loginCard = document.querySelector('.flipBox__card_login'),
+      homeBtn = document.querySelector('.home');
+
+    autBtn.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      if (autBtn.classList.contains('authorization_hidden')) {
+        autBtn.classList.remove('authorization_hidden');
+      } else {
+        autBtn.classList.add('authorization_hidden');
+
+        flipBox.style.transform = "rotateY(180deg)";
+        userCard.classList.remove('flipBox__card_user_active');
+        loginCard.classList.add('flipBox__card_login_active');
+      }
+    })
+
+    homeBtn.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      if (autBtn.classList.contains('authorization_hidden')) {
+        autBtn.classList.remove('authorization_hidden');
+      }
+
+      flipBox.style.transform = "rotateY(0deg)";
+    })
+
+  }
+
+});
